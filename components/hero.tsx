9f84b8ead24b164bece5ac5d92bfa4256976c3cf
Jack Michell <me@jmich.dev>
2024-06-30 21:16:52 -0500

    feat: Initial project setup (everything else)

---
+++ components/hero.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import TextLogo from "../public/assets/text.png";
+import { AiOutlineMail } from "react-icons/ai";
+
+export default function Hero() {
+  const visible = { opacity: 1, y: 0, transition: { duration: 0.8 } };
+  const hidden = { opacity: 0, y: -100 };
+
+  return (
+    <AnimatePresence>
+      <motion.article
+        initial="hidden"
+        animate="visible"
+        exit={{ opacity: 0, transition: { duration: 1 } }}
+      >
+        <motion.h1
+          variants={{ hidden, visible }}
+          className="text-center text-6xl sm:text-8xl lg:text-9xl font-bold text-[#5651e5]"
+        >
+          <Image src={TextLogo} alt="Mosby's Land Management Service" />
+        </motion.h1>
+        <motion.p
+          variants={{ hidden, visible }}
+          className="text-center text-muted-foreground text-sm sm:text-md lg:text-xl"
+        >
+          We provide professional land management services for residential and
+          commercial properties.
+        </motion.p>
+        <motion.div className="w-full flex items-center justify-center py-4 gap-8">
+          <motion.div
+            variants={{ hidden, visible }}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.8 }}
+          >
+            <Link href="/contact" rel="noreferrer">
+              <div className="rounded-full shadow-lg p-6 cursor-pointer">
+                <AiOutlineMail size={20} />
+              </div>
+            </Link>
+          </motion.div>
+        </motion.div>
+      </motion.article>
+    </AnimatePresence>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../public/assets/logo.png";
+import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <div className="fixed z-50 bg-background w-full py-2">
+      <div className="flex justify-start items-center mx-8 gap-6">
+        <Link href="/">
+          <Image src={Logo} alt="Logo" width={50} height={50} priority />
+        </Link>
+        <Link href="/">
+          <Button variant="outline" className="uppercase hover:text-red-400">
+            Home
+          </Button>
+        </Link>
+        <Link href="/services">
+          <Button variant="outline" className="uppercase hover:text-red-400">
+            Services
+          </Button>
+        </Link>
+        <Link href="/contact">
+          <Button variant="outline" className="uppercase hover:text-red-400">
+            Contact
+          </Button>
+        </Link>
+        <div className="fixed right-4">
+          <ModeToggle />
+        </div>
+      </div>
+    </div>
+  );
+}
